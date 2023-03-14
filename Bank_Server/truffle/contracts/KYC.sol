34dbc45eec7eeaddc5f43510719d0d0f692d97c1
Slutishboy <83270202+Slutishboy@users.noreply.github.com>
2023-03-15 01:02:50 +0530

    updated_code

--- Bank_Server/truffle/contracts/KYC.sol
+++ Bank_Server/truffle/contracts/KYC.sol
@@ -2,11 +2,12 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract SimpleStorage {
-    uint256 value;
     address private contractOwner;
-    address contractAddr;
-    constructor(address publisher) public {
-      contractOwner=publisher;
+    uint private uniqueID ;
+    // address contractAddr;
+    constructor() public {
+      contractOwner=msg.sender;
+      uniqueID=100001;
    }
     struct Customer {
       string name;
@@ -18,15 +19,39 @@ contract SimpleStorage {
       uint panNumber;
       string gender;
       address userAccountAddress;
-      string UploadDate;
+      string uploadDate;
       uint phone;
 
    }
-    function read() public view returns (uint256) {
-        return value;
+     mapping (uint => Customer) public product;
+
+
+      function addData(string memory  date,string memory  name, string memory fatherName,string memory motherName, uint adharNumber,string memory dateOfBirth,string memory addr,
+      uint panNumber,string memory gender,address userAccountAddress,uint phone) public returns(uint){    
+          Customer memory p = Customer(name,fatherName,motherName,dateOfBirth,addr,adharNumber,panNumber,gender,userAccountAddress,date,phone);
+                product[uniqueID]=p;
+               
+                uniqueID+=1;
+            
+            return (uniqueID-1);
+       
     }
 
-    function write(uint256 newValue) public {
-        value = newValue;
-    }
+    function getData(uint id) public view returns( string memory  date,string memory  name, string memory fatherName,string memory motherName, uint adharNumber,string memory dateOfBirth,string memory addr,uint panNumber,string memory gender,address userAccountAddress,uint phone ){
+        return(
+            product[id].uploadDate,
+            product[id].name,
+            product[id].fatherName,
+            product[id].motherName,
+            product[id].adharNumber,
+            product[id].dateOfBirth,
+            product[id].addr,
+            product[id].panNumber,
+            product[id].gender,
+            product[id].userAccountAddress,
+            product[id].phone
+            );
+
+      }
+
 }
