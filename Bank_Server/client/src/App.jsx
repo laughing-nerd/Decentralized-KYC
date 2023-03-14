f3579bd705d789f5aeb1b674322aa51ef7d1903e
Gairik Sharma <gairiksharma202@gmail.com>
2023-03-14 22:30:47 +0530

    screen 1- sending kyc requests to the customers

--- Bank_Server/client/src/App.jsx
+++ Bank_Server/client/src/App.jsx
@@ -1,24 +1,17 @@
-import { EthProvider } from "./contexts/EthContext";
+// import { EthProvider } from "./contexts/EthContext";
 import Intro from "./components/Intro/";
 import Setup from "./components/Setup";
 import Demo from "./components/Demo";
 import Footer from "./components/Footer";
+import Navbar from "./components/navbar/navbar";
+import AdminSentReq from "./pages/adminSentReq";
 
 function App() {
   return (
-    <EthProvider>
-      <div id="App">
-        <div className="container">
-          <Intro />
-          <hr />
-          <Setup />
-          <hr />
-          <Demo />
-          <hr />
-          <Footer />
-        </div>
+      <div className="App">
+        {/* <Navbar /> */}
+        <AdminSentReq />
       </div>
-    </EthProvider>
   );
 }
 
